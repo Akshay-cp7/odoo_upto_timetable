dd08108bdcda0f62dc466faa51354b1dfc9b0ae8
Akshay C P <com.akshaycp@gmail.com>
2024-10-21 13:27:14 +0530

    Add files via upload

---
+++ test2/models/faculty.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+class Faculty(models.Model):
+    _name = 'fk.faculty'
+    _description = 'Faculty'
+
+    name = fields.Char(string='Name', required=True)
+    code = fields.Char(string='Code')
+    dept_id = fields.Many2one(
+        'fk.dept', 
+        string='Department', 
+        required=True
+    )
